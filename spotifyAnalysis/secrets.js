a965b78c3a2ae38ec226ffd8cf1746c7bbf9bd5e
tamara-h <tlh537@cs.bham.ac.uk>
2019-08-06 23:05:56 +0100

    remove and update client secrets

--- spotifyAnalysis/secrets.js
+++ spotifyAnalysis/secrets.js
@@ -2,7 +2,7 @@
 // These should not be released outside of marking the final project
 // if uploading to an external source, uncomment the line in the .gitignore to remove this
 // exports.spotifyCreds = {clientId: 'cf80afba582945c5ab022ce3e8d4fb50', clientSecret: '5a113d6c09c34f88a1934c735a561696'};
-exports.spotifyCreds = {clientId: '2ef21279418442ca8807da295adbe1da', clientSecret: '847583e8dcde4ceeae894e5d679214e7'};
+exports.spotifyCreds = {clientId: '2ef21279418442ca8807da295adbe1da', clientSecret: 'bd53ab2e6511435bb291769f94e1a59b'};
 
 // exports.spotifySourcePlaylist = {userId: '1137728959', playlistId: '4tYprYIWkYXKs3c2cOe19v'};
 
