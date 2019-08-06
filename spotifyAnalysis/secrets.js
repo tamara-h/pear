39b8942e34aa03a03465217c4e54082dac17e8ba
tamara-h <tlh537@cs.bham.ac.uk>
2019-08-06 22:45:43 +0100

    add secrets lol I WILL REMOVE THIS IN FUTURE DONT WORRY

---
+++ spotifyAnalysis/secrets.js
@@ -0,0 +1,9 @@
+//THESE ARE MY CLIENT SECRETS
+// These should not be released outside of marking the final project
+// if uploading to an external source, uncomment the line in the .gitignore to remove this
+// exports.spotifyCreds = {clientId: 'cf80afba582945c5ab022ce3e8d4fb50', clientSecret: '5a113d6c09c34f88a1934c735a561696'};
+exports.spotifyCreds = {clientId: '2ef21279418442ca8807da295adbe1da', clientSecret: '847583e8dcde4ceeae894e5d679214e7'};
+
+// exports.spotifySourcePlaylist = {userId: '1137728959', playlistId: '4tYprYIWkYXKs3c2cOe19v'};
+
+module.exports = exports;
